f11b3a89666e7eb443e27d21fa728932bfff821a
BraNanaK <princesamuelpks@gmail.com>
2021-09-17 06:43:01 +0000

    Loaded static files

--- ImageVirtual/Project/Project/settings.py
+++ ImageVirtual/Project/Project/settings.py
@@ -1,5 +1,7 @@
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,6 +110,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'imageUpload')
+MEDIA_URL = '/imageUpload/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
